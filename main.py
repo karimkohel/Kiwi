2374f42de348d793e6782f6aff64b7d01a090cc5
karimkohel <kareemkohel@gmail.com>
2021-03-27 00:17:07 +0200

    setting up shop

--- main.py
+++ main.py
@@ -4,11 +4,35 @@ import pickle
 import numpy as np
 
 import nltk
-from nltk.stem import  WordNetLemmatizer as lemma
+from nltk.stem import  WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
+nltk.download('punkt')
+nltk.download('wordnet')
 
+lem = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
 
-print("testing")
+words = []
+classes = []
+documents = []
+ignoreLetters = ['!', '?', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        wordList = nltk.word_tokenize(pattern)
+        words.extend(wordList)
+        documents.append((wordList, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lem.lemmatize(word) for word in words if word not in ignoreLetters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
