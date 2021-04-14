54ca4e5e3d745f7a38a14e96da3a0a78993eda29
karimkohel <kareemkohel@gmail.com>
2021-04-14 17:03:05 +0200

    ability to get weather from API

--- fx.py
+++ fx.py
@@ -1,17 +1,20 @@
 from datetime import datetime
-
-def test():
-    print('--- THE TEST FC RAN')
+import requests
 
 def getTime():
     time = datetime.now().time()
     time = time.strftime("%I:%M %p")
     print("It's ", time)
 
-
+def getWeather():
+    api = 'http://api.openweathermap.org/data/2.5/weather?q=Cairo&appid=04a716d70b54bf5c6c24dbb3dfa5db03&units=metric'
+    allData = requests.get(api).json()
+    weather = allData['weather'][0]['description']
+    temp = allData['main']['temp']
+    print(weather, ", ", temp)
 
 
 mappings = {
-    'greeting' : test,
     'time' : getTime,
+    'weather' : getWeather
 }
