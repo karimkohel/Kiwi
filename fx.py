e9184e6488e9538b27d94f4c8295f1ceb7f50069
karimkohel <kareemkohel@gmail.com>
2021-04-15 02:17:40 +0200

    dynamix responses for custom functions

--- fx.py
+++ fx.py
@@ -4,19 +4,19 @@ import random
 
 import speech
 
-def getTime():
+def getTime(intent):
     time = datetime.now().time()
     time = time.strftime("%I:%M %p")
     speech.speak("It's " + time)
 
-def getWeather():
+def getWeather(intent):
     api = 'http://api.openweathermap.org/data/2.5/weather?q=Cairo&appid=04a716d70b54bf5c6c24dbb3dfa5db03&units=metric'
     allData = requests.get(api).json()
     weather = allData['weather'][0]['description']
     temp = allData['main']['temp']
     speech.speak(weather, ", ", temp)
 
-def takeNotes():
+def takeNotes(intent):
     speech.speak("Ready to take your notes")
     note = speech.takeCommand()
     with open("note.txt", 'a') as f:
@@ -24,9 +24,8 @@ def takeNotes():
         f.write("\n---------------------\n")
     speech.speak("Ok done")
 
-def goodbye():
-    l = ["Bye", "Au revoir", "Hope to see you soon", "Talk later", "Ok Goodbye!", "Goodbye then", "Ok I'll go now", "see you later"]
-    speech.speak(random.choice(l))
+def close(intent):
+    speech.speak(intent)
     exit(0)
 
 
@@ -34,5 +33,5 @@ mappings = {
     'time' : getTime,
     'weather' : getWeather,
     'note' : takeNotes,
-    'goodbye': goodbye
+    'goodbye': close
 }
