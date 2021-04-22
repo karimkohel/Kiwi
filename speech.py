77815fd031c6526767c90ec6d04d70809ec286b4
karimkohel <kareemkohel@gmail.com>
2021-04-22 15:19:58 +0200

    built the wait on wake up call to wait for hey poppy

--- speech.py
+++ speech.py
@@ -16,7 +16,7 @@ def speak(text):
     speaker.runAndWait()
 
 def takeCommand():
-    recognizer = sr.Recognizer();
+    recognizer = sr.Recognizer()
 
     while True:
         try:
@@ -32,4 +32,30 @@ def takeCommand():
             speak("Sorry I couldn't get that, can you try again?")
         except Exception:
             print("error in take command")
+            exit(1)
+
+
+def waitForWakeupCall(text):
+
+    recognizer = sr.Recognizer()
+
+    while True:
+        try:
+            with sr.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.3)
+                audio = recognizer.listen(mic)
+
+                audio = recognizer.recognize_google(audio)
+                audio = audio.lower()
+
+                if audio.find(text) > 0:
+                    speak("hey nada")
+                    break
+                else:
+                    continue
+
+        except sr.UnknownValueError:
+            continue
+        except Exception:
+            print("error in wait wake up call")
             exit(1)
