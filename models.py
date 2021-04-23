cad727d0a0def45a4099aec472dc7994e9b43ffd
karimkohel <kareemkohel@gmail.com>
2021-04-23 02:10:50 +0200

    finished models

--- models.py
+++ models.py
@@ -1,9 +1,33 @@
+import pickle
+
 
 class Reminder():
-    def __init__(self, reminder, dueDate):
-        self.reminder = reminder
+    def __init__(self, text, dueDate):
+        self.text = text
         self.dueDate = dueDate
 
+    @staticmethod
+    def readReminders(file = "reminder.p"):
+        try:
+            reminders = pickle.load(open(file, 'rb'))
+        except FileNotFoundError:
+            reminders = []
+        return reminders
+
+    @staticmethod
+    def writeReminders(reminders, file = 'reminder.p'):
+        pickle.dump(reminders, open(file, 'wb'))
+
+
 class note():
-    def __init__(self, note):
-        self.note = note
+    def __init__(self, text):
+        self.note = text
+
+
+    @staticmethod
+    def readReminders():
+        return pickle.load(open("reminder.p", 'rb'))
+
+    @staticmethod
+    def writeReminders(reminders):
+        pickle.dump(reminders, open('reminder.p', 'wb'))
