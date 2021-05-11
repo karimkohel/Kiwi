c2e7506b51d099caa23a775bafc231e59ef3d19b
karimkohel <kareemkohel@gmail.com>
2021-05-11 19:05:51 +0200

    handling basic UI with threading

--- poppy.py
+++ poppy.py
@@ -1,45 +1,51 @@
 from neuralintents import GenericAssistant
 from fx import mappings
 import speech
-
-assistant = GenericAssistant('intents.json', model_name="test_model", intent_methods=mappings)
-assistant.train_model()
-assistant.save_model()
-# assistant.load_model()
-
-
-while True:
-
-    speech.waitForWakeupCall("hey")
-    working = True
-    counter = 0
-
-    while working:
-        counter += 1
-        message = speech.takeCommand()
-
-        if counter > 5:
-            message = "enough work for today"
-            working = assistant.request(message)
-            break
-        else:
-            working = assistant.request(message)
-
-    if "exit" in message:
-        break
-
 from tkinter import *
 from PIL import ImageTk,Image
+import threading, sys
+
+
+assistant = GenericAssistant('intents.json', model_name="test_model", intent_methods=mappings)
+# assistant.train_model()
+# assistant.save_model()
+assistant.load_model()
+
+
+def mainLoop(app):
+
+    while True:
+
+        speech.waitForWakeupCall("hey")
+        working = True
+        counter = 0
+
+        while working:
+            counter += 1
+            message = speech.takeCommand()
+
+            if counter > 5:
+                message = "enough work for today"
+                working = assistant.request(message)
+                break
+            else:
+                working = assistant.request(message)
+
+        if "exit" in message:
+            break
+    app.destroy()
 
 root = Tk()
 root.title('Kiwi')
 root.iconbitmap('icon.ico')
-
-
 my_img1 = ImageTk.PhotoImage(Image.open("kiwi.jpg"))
-
-
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
 
+
+proc = threading.Thread(target=mainLoop, args=(root,))
+proc.daemon = True
+proc.start()
+
 root.mainloop()
+sys.exit(0)
